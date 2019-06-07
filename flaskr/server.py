08e5d75b5e68fbcc192057be23f686b6fcbf617f
artslob <artslob@yandex.ru>
2019-06-07 15:08:17 +0300

    add route for single server

--- flaskr/server.py
+++ flaskr/server.py
@@ -12,3 +12,13 @@ def all_servers():
     columns = ('id', 'created', 'changed', 'rack_id')
     result = [{name: getattr(rack, name, '<empty>') for name in columns} for rack in rows]
     return jsonify(result)
+
+
+@bp.route('/<int:id>', methods=('GET',))
+def get_rack(id):
+    server = Server.query.get(id)
+    if not server:
+        return jsonify({}), 404
+
+    columns = ('id', 'created', 'changed', 'rack_id')
+    return jsonify({name: getattr(server, name, '<empty>') for name in columns})
