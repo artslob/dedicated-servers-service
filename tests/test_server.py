b12efb8b439892b70de524e7883f9fbe6c226a8f
artslob <artslob@yandex.ru>
2019-06-07 17:47:59 +0300

    check count of servers

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,5 +1,9 @@
+import json
+
+
 def test_hello(client, default_data):
     response = client.get('/server/all')
     assert response._status_code == 200
     assert response.content_type == 'application/json'
-    assert len(response.data) > 10
+    servers = json.loads(response.data)
+    assert len(servers) == 25
